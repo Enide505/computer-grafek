f6e05ee54881d7eb8dd635f6d9083f9508b7de76
Enide505 <sharaev.dinislam@yandex.ru>
2024-09-18 14:55:08 +0300

    made canvas global

--- main.py
+++ main.py
@@ -4,12 +4,13 @@ from tkinter import *
 from tkinter.ttk import Style
 
 unitSize = 18
-
 class Example(Frame):
-
     def __init__(self, parent):
         Frame.__init__(self, parent, background="white")
+
         self.parent = parent
+        self.canvas = Canvas(self.parent, width=800, height = 800)
+
         self.parent.title("Window")
         self.pack(fill=BOTH, expand=1) #для центровки окна
         self.centerWindow()
@@ -17,44 +18,47 @@ class Example(Frame):
 
         self.InitUI()
 
+
     def draw_axis(self, x_left, x_right, y_bottom, y_top):
         sw = self.parent.winfo_screenwidth()  # Ширина, высота экрана
         sh = self.parent.winfo_screenheight()
 
         # Блок оси и их масштаб
-        canvas = Canvas(self)
+        # canvas = Canvas(self)
+
         # canvas.create_line(0, sh / 2, sw, sh / 2)
         # canvas.pack(fill=BOTH, expand=1)
-        canvasWidth = canvas.winfo_screenwidth()-50
-        canvasHeight = canvas.winfo_screenheight()-50
+
 
         # canvas.create_line(sw / 2, 0, sw / 2, sh)
-        canvas.pack(fill=BOTH, expand=1)
-
+        self.canvas.pack(fill=BOTH, expand=1)
+        canvasWidth = self.canvas.winfo_screenwidth()
+        canvasHeight = self.canvas.winfo_screenheight()
         dx = canvasWidth / (x_right - x_left)
         dy = canvasHeight / (y_top - y_bottom)
 
         cx = -x_left * dx
         cy = y_top * dy
 
-        canvas.create_line(0, cy, canvasWidth, cy, fill='black')
-        canvas.create_line(cx, 0, cx, canvasHeight, fill='black')
+        self.canvas.create_line(0, cy, canvasWidth, cy, fill='black')
+        self.canvas.create_line(cx, 0, cx, canvasHeight, fill='black')
 
         x_step = (x_right - x_left) / 18
         x = x_left
         while x <= x_right:
             x_canvas = (x - x_left) * dx
-            canvas.create_line(x_canvas, cy - 3, x_canvas, cy + 3, fill='black')
-            canvas.create_text(x_canvas, cy + 15, text=str(round(x, 1)), font="Verdana 9", fill='black')
+            self.canvas.create_line(x_canvas, cy - 3, x_canvas, cy + 3, fill='black')
+            self.canvas.create_text(x_canvas, cy + 15, text=str(round(x, 1)), font="Verdana 9", fill='black')
             x += x_step
 
         y_step = (y_top - y_bottom) / 18
         y = y_top
         while y >= y_bottom:
             y_canvas = (y - y_top) * dy
-            canvas.create_line(cx - 3, -y_canvas, cx + 3, -y_canvas, fill='black')
-            canvas.create_text(cx + 25, -y_canvas, text=str(round(y, 1)), font="Verdana 9", fill='black')
+            self.canvas.create_line(cx - 3, -y_canvas, cx + 3, -y_canvas, fill='black')
+            self.canvas.create_text(cx + 25, -y_canvas, text=str(round(y, 1)), font="Verdana 9", fill='black')
             y -= y_step
+
 
 
 
@@ -92,6 +96,8 @@ class Example(Frame):
         def getShapeCoords():
             global shapeCoords
             shapeCoords = list(map(int, entry1.get().split()))
+            for i in shapeCoords:
+                i*=dx
             print(shapeCoords)
 
         def getLineCoords():
@@ -120,6 +126,7 @@ class Example(Frame):
         # entry = ttk.Entry(self)
         # entry.pack(side = RIGHT, padx=5, pady=5)
 #----------------------------------------------------------------------------
+
         frame1 = Frame(self,relief="raised",borderwidth=1)
         frame1.pack(fill=X)
         lbl1 = Label(frame1, text="Координаты вершин объекта", width=25)
@@ -137,6 +144,7 @@ class Example(Frame):
         entry2.pack(side = LEFT, padx=5)#, expand=True)
         okButton2 = Button(frame2, text="Готово", command=getLineCoords)
         okButton2.pack(side=LEFT, padx=10, pady=5)
+
 
 
 def main():
