deafe7d415f795d53018e40ec278eede2b40a7b2
Enide505 <sharaev.dinislam@yandex.ru>
2024-09-29 15:08:50 +0300

    add GoToZ, GoToInit, Reflect functions

--- main.py
+++ main.py
@@ -1,30 +1,31 @@
 # from array import array
+from math import atan2, degrees
 from tkinter import *
 # from tkinter import ttk
 from tkinter.ttk import Style
 
 unitSize = 18
+
+
 class Example(Frame):
     def __init__(self, parent):
         Frame.__init__(self, parent, background="white")
 
         self.parent = parent
-        self.canvas = Canvas(self.parent, width=800, height = 800)
+        self.canvas = Canvas(self.parent, width=800, height=800)
         self.canvasWidth = self.canvas.winfo_screenwidth()
         self.canvasHeight = self.canvas.winfo_screenheight()
 
         self.parent.title("Window")
-        self.pack(fill=BOTH, expand=1) #для центровки окна
+        self.pack(fill=BOTH, expand=1)  # для центровки окна
         self.centerWindow()
-        self.dx=0
-        self.dy=0
-        self.cx=0
-        self.cy=0
+        self.dx = 0
+        self.dy = 0
+        self.cx = 0
+        self.cy = 0
         self.draw_axis(-6, 12, -6, 12)
 
-
         self.InitUI()
-
 
     def draw_axis(self, x_left, x_right, y_bottom, y_top):
         sw = self.parent.winfo_screenwidth()  # Ширина, высота экрана
@@ -35,7 +36,6 @@ class Example(Frame):
 
         # canvas.create_line(0, sh / 2, sw, sh / 2)
         # canvas.pack(fill=BOTH, expand=1)
-
 
         # canvas.create_line(sw / 2, 0, sw / 2, sh)
         self.canvas.pack(fill=BOTH, expand=1)
@@ -66,61 +66,101 @@ class Example(Frame):
             self.canvas.create_text(self.cx + 25, -y_canvas, text=str(round(y, 1)), font="Verdana 9", fill='black')
             y -= y_step
 
-
-
-
     def centerWindow(self):
-        w = 1280                         #ширина, высота окна
+        w = 1280  # ширина, высота окна
         h = 720
 
-        sw = self.parent.winfo_screenwidth()        #Ширина, высота экрана
+        sw = self.parent.winfo_screenwidth()  # Ширина, высота экрана
         sh = self.parent.winfo_screenheight()
 
         x = (sw - w) / 2
         y = (sh - h) / 2
         self.parent.geometry('%dx%d+%d+%d' % (w, h, x, y))
 
-
-
         # canvas.create_polygon(
         #     points, outline='red',
         #     fill='green', width=2
         # )
 
-
-
-
-
-
     # entry = ttk.Entry()
     # entry.pack(anchor=NW, padx=16, pady=16)
 
-
-
     def InitUI(self):
 
-        shapeCoords=0
+        # shapeCoords=0
         def getShapeCoords():
             global shapeCoords
             shapeCoords = list(map(int, entry1.get().split()))
-            for i in range (0,len(shapeCoords),2):
+            for i in range(0, len(shapeCoords), 2):
                 shapeCoords[i] *= self.dx
-                shapeCoords[i]+=self.cx
+                shapeCoords[i] += self.cx
 
                 # shapeCoords[i]
             for i in range(1, len(shapeCoords), 2):
                 shapeCoords[i] *= -self.dy
-                shapeCoords[i]+=self.cy
+                shapeCoords[i] += self.cy
             # shapeCoords[i]
             print(shapeCoords)
-            self.canvas.create_polygon(shapeCoords, outline ='green', fill='green')
+            self.canvas.create_polygon(shapeCoords, outline='green', fill='green')
 
         def getLineCoords():
             global lineCoords
             lineCoords = list(map(int, entry2.get().split()))
+            for i in range(0, len(lineCoords), 2):
+                lineCoords[i] *= self.dx
+                lineCoords[i] += self.cx
+
+                # shapeCoords[i]
+            for i in range(1, len(lineCoords), 2):
+                lineCoords[i] *= -self.dy
+                lineCoords[i] += self.cy
+            print(lineCoords)
+            self.canvas.create_line(lineCoords)
+            # self.canvas.delete("all")
+
+        deltaX, deltaY = 0, 0
+
+        def GoToZ():
+            global shapeCoords
+            global deltaX, deltaY
+            deltaX = lineCoords[0]
+            deltaY = lineCoords[1]
+            print("FFF", deltaX, deltaY)
+
+            for i in range(0, len(lineCoords), 2):
+                lineCoords[i] -= deltaX
+            for i in range(1, len(lineCoords), 2):
+                lineCoords[i] -= deltaY
+
+            for i in range(0, len(shapeCoords), 2):
+                shapeCoords[i] -= deltaX
+            for i in range(1, len(shapeCoords), 2):
+                shapeCoords[i] -= deltaY
             print(lineCoords)
 
+        def GoToInit():
+            global deltaX, deltaY
+            for i in range(0, len(lineCoords), 2):
+                lineCoords[i] += deltaX
+            for i in range(1, len(lineCoords), 2):
+                lineCoords[i] += deltaY
 
+            for i in range(0, len(shapeCoords), 2):
+                shapeCoords[i] += deltaX
+            for i in range(1, len(shapeCoords), 2):
+                shapeCoords[i] += deltaY
+            print("ALOOO ", deltaX, deltaY)
+
+        def RotateToX():
+            angle = 0
+            angle = atan2(lineCoords[3] - lineCoords[1], lineCoords[2] - lineCoords[0])
+            print("angle", angle)
+            print("deg", degrees(angle))
+
+        def Reflect():
+            GoToZ()
+            RotateToX()
+            GoToInit()
 
         self.master.title("Lab1")
         self.style = Style()
@@ -130,8 +170,6 @@ class Example(Frame):
         # frame.pack(fill=BOTH, expand=True)                         # что-то не то
         # self.pack(fill=BOTH, expand=True)
 
-
-
         # closeButton = Button(self, text="Закрыть")
         # closeButton.pack(side=RIGHT, padx=5, pady=5)
         # okButton = Button(self, text="Готово", command=getData)
@@ -140,14 +178,14 @@ class Example(Frame):
         # label.pack(side=RIGHT, padx=5, pady=5)
         # entry = ttk.Entry(self)
         # entry.pack(side = RIGHT, padx=5, pady=5)
-#----------------------------------------------------------------------------
+        # ----------------------------------------------------------------------------
 
-        frame1 = Frame(self,relief="raised",borderwidth=1)
+        frame1 = Frame(self, relief="raised", borderwidth=1)
         frame1.pack(fill=X)
         lbl1 = Label(frame1, text="Координаты вершин объекта", width=25)
         lbl1.pack(side=LEFT, padx=5, pady=5)
         entry1 = Entry(frame1)
-        entry1.pack(side = LEFT, padx=5)#, expand=True)
+        entry1.pack(side=LEFT, padx=5)  # , expand=True)
         okButton1 = Button(frame1, text="Готово", command=getShapeCoords)
         okButton1.pack(side=LEFT, padx=10, pady=5)
 
@@ -156,10 +194,12 @@ class Example(Frame):
         lbl2 = Label(frame2, text="Координаты точек прямой", width=25)
         lbl2.pack(side=LEFT, padx=5, pady=5)
         entry2 = Entry(frame2)
-        entry2.pack(side = LEFT, padx=5)#, expand=True)
+        entry2.pack(side=LEFT, padx=5)  # , expand=True)
         okButton2 = Button(frame2, text="Готово", command=getLineCoords)
         okButton2.pack(side=LEFT, padx=10, pady=5)
 
+        okButton3 = Button(frame2, text="Отразить", command=Reflect)
+        okButton3.pack(side=LEFT, padx=10, pady=5)
 
 
 def main():
